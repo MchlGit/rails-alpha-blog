90d6546ec23cb6d5370a1d6ebe74a24915c2e0a4
MchlGit <60210564+MchlGit@users.noreply.github.com>
2025-11-06 17:52:52 -0800

    Add user migration file

---
+++ db/migrate/20251107014653_add_to_user_table.rb
@@ -0,0 +1,5 @@
+class AddToUserTable < ActiveRecord::Migration[8.1]
+  def change
+    add_column :users, :email, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_07_010639) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_07_014653) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at"
     t.text "description"
@@ -21,6 +21,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_07_010639) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.string "email"
     t.datetime "updated_at", null: false
     t.string "username"
   end
