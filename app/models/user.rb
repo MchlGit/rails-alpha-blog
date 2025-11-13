a2329cf9b262f7fe8c15fcec13cfe9c08ac78bc6
MchlGit <60210564+MchlGit@users.noreply.github.com>
2025-11-12 16:06:52 -0800

    Added user edit functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,5 @@ class User < ApplicationRecord
             length: { maximum: 105 },
             format: { with: VALID_EMAIL_REGEX }
   has_secure_password
+  validates :password, length: { minimum: 6 }, allow_nil: true
 end
