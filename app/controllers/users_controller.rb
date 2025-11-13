3ce5c6b1fc8ab177897e6b070dc4028f9d696de5
MchlGit <60210564+MchlGit@users.noreply.github.com>
2025-11-12 17:25:12 -0800

    Add user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
   # GET /users/new
   def new
     @user = User.new
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,18 @@
+require "digest"
+require "uri"
 module UsersHelper
+  def gravatar_for(user, options = { size: 80})
+    email_address = user.email.downcase
+
+    # Create the SHA256 hash
+    hash = Digest::SHA256.hexdigest(email_address)
+
+    # Set default URL and size parameters
+    default = "https://www.example.com/default.jpg"
+    size = options[:size]
+    # Compile the full URL with URI encoding for the parameters
+    params = URI.encode_www_form("d" => default, "s" => size)
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?#{params}"
+    image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
+  end
 end
