e65de082c1371e0dd914ce138fb268843fb2dc6f
MchlGit <60210564+MchlGit@users.noreply.github.com>
2025-11-25 15:11:32 -0800

    Added in sessions

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,10 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   resources :articles
   get "signup", to: "users#new"
-  resources :users, except: [ :new ]
+  resources :users, except: [:new]
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "login", to: "sessions#destroy"
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
